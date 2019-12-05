d41543472f60f9b3ae9c2192fcd0e6adb5ac14e9
Harambasa <ilijammilosavljevic@gmail.com>
2019-12-05 16:08:53 +0100

    font awesome 4 radi, 5 nece

--- BackMeUpApp/ClientApp/src/index.js
+++ BackMeUpApp/ClientApp/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import "normalize.css/normalize.css"; //koristi se da normalizuje css na razlicitim browser-ima
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/styles.scss";
-import '../node_modules/font-awesome/css/font-awesome.css'; 
+//import "../node_modules/font-awesome/css/font-awesome.css";
 
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
@@ -14,14 +14,19 @@ const store = configureStore();
 
 const jsx = (
   <>
-  <Provider store={store}>
-    <AppRouter />
-    
-  </Provider>
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
 
-
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" type="text/css"/>
-</>
+    <script
+      src="https://kit.fontawesome.com/36dc9f06c1.js"
+      crossOrigin="anonymus"
+    ></script>
+    <link
+      rel="stylesheet"
+      href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+  </>
 );
 
 ReactDOM.render(jsx, document.getElementById("root"));
